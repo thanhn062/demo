b8b467e7d794cd4cd6c5f0fea2f9892455f31fd1
thanhn062 <510thanh.ngo@gmail.com>
2017-03-13 00:23:42 -0700

    Update expense_data.php

--- expense_data.php
+++ expense_data.php
@@ -1,6 +1,6 @@
 <?php
 // Login to mySQL
-$link = mysqli_connect("127.0.0.1", "root", "dell14789632", "berkeleynails");
+$link = mysqli_connect("127.0.0.1", "my_id", "my_pw", "my_db");
 if (!$link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
